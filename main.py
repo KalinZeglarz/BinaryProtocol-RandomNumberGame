9920151d9509a37d5f73ee35c1b2741a137196fe
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-15 15:14:01 +0100

    Menu Created

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+def User():
+    print("I'm User!")
+
+def Server():
+    print("I'm Server!")
+
 def menu():
     loop = 1
     start = 0
     while loop == 1:
         start = input("Choose working mode (Server-1, User-2, Exit-0):")
         if start == 1:
-            print()
+            Server()
+            loop = 0
         elif start == 2:
-            print()
+            User()
+            loop = 0
         elif start == 0:
-            return 0
+            loop = 0
         else:
             print("Wrong choose. Choose again (type 1 for \"Server\" or 2 for \"User\" or 0 for Exit)")
 
