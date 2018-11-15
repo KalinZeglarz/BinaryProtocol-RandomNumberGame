21d59ff4357a16e6fbe97d83f51259adba62d655
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-15 14:57:39 +0100

    Test1

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-start = input("Choose working mode (Server or User): ")
+def menu():
+    loop = 1
+    start = 0
+    while loop == 1:
+        start = input("Choose working mode (Server-1, User-2, Exit-0): ")
+        if start == 1:
+            print()
+        elif start == 2:
+            print()
+        elif start == 0:
+            return 0
+        else:
+            print("Wrong choose. Choose again (type 1 for \"Server\" or 2 for \"User\" or 0 for Exit)")
 
-if start == "Server":
-    print()
-else:
-    if start == "User":
-        print()
-    else:
-        start = input("Wrong choose. Choose again (type \"Server\" or \"User\"): ")
+menu()
