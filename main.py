5cef66a545ed046ac47adbe723d4719854764bd4
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-15 14:44:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+start = input("Choose working mode (Server or User): ")
+
+if start == "Server":
+    print()
+else:
+    if start == "User":
+        print()
+    else:
+        start = input("Wrong choose. Choose again (type \"Server\" or \"User\"): ")
