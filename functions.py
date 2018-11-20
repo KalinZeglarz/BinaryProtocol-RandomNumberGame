221eb82621b8ae4ac086c22ae205ce3266c12d61
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-21 00:41:58 +0100

    Added console help and args parsing

--- functions.py
+++ functions.py
@@ -8,3 +8,11 @@ def boolArrTOint(boolArr):
         w = w * 2 + int(x)
     return w
 
+#Additional function (not used) for understanding of binary conversion
+def binarytoint(b):
+    w=0
+    string = b
+    for x in string:
+        w = w*2+x
+    return w
+
